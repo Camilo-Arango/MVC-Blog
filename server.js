33c51220ecfcf46432e99b0c6ba5a380beedb14b
Camilo Arango <camilo94@icloud.com>
2022-08-30 01:00:35 -0700

    asdfasd

--- server.js
+++ server.js
@@ -1,41 +1,64 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// Server for MVC Tech Blog
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Dependencies
+// path module
+const path = require("path");
+// dotenv file for sensitive configuration information
+require("dotenv").config();
+// Express.js server
+const express = require("express");
+// All routes as defined in the controllers folder
+const routes = require("./controllers/");
+// Sequelize connection to the database
+const sequelize = require("./config/connection");
+// Handlebars template engine for front-end
+const exphbs = require("express-handlebars");
+// Express session to handle session cookies
+const session = require("express-session");
+// Sequelize store to save the session so the user can remain logged in
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// Handlebars helpers
+const helpers = require("./utils/helpers");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
+// Initialize handlebars for the html templates
 const hbs = exphbs.create({ helpers });
 
+// Initialize sessions
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: { maxAge: 7200000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
-app.use(session(sess));
+// Initialize the server
+const app = express();
+// Define the port for the server
+const PORT = process.env.PORT || 3001;
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// Give the server a path to the public directory for static files
+app.use(express.static(path.join(__dirname, "public")));
 
+// Set handlebars as the template engine for the server
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Have Express parse JSON and string data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Tell the app to use Express Session for the session handling
+app.use(session(sess));
+
+// Give the server the path to the routes
 app.use(routes);
 
+// Turn on connection to db and then to the server
+// force: true to reset the database and clear all values, updating any new relationships
+// force: false to maintain data - aka normal operation
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
